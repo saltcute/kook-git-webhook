ab81ded7aeb10823b905e4fcdd336c2b8d18b063
saltcute <me@mizuno.ai>
2024-06-19 15:54:36 -0400

    feat: use unique and random iv

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,44 @@ import upath from 'upath';
 import crypto from 'crypto';
 
 import { addWebhooks } from "api";
+import { getIV, IV } from "menu/gitnya/lib";
 
 (async () => {
-    const list = await client.config.getOne('gitnya::webhooks.channel.list') || [];
+    let list = await client.config.getOne('gitnya::webhooks.channel.list') || [];
     for (const UUID of list) {
         const store = await client.config.getOne(`gitnya::webhooks.channel.mappings.${UUID}`);
-        if (!store) continue;
-        const { channelId, secret } = store;
+        if (!store) {
+            list = list.filter(v => v !== UUID);
+            continue;
+        }
+        let { channelId, secret, iv, repo } = store;
+        if (!iv) {
+            const decrypted = crypto
+                .createDecipheriv('aes-256-gcm',
+                    (await client.config.getOne('gitnya::webhooks.key')).padEnd(32),
+                    IV)
+                .update(Buffer.from(secret, 'base64'))
+                .toString('utf8');
+            client.config.set(`gitnya::webhooks.channel.mappings.${UUID}`, undefined);
+            client.config.set(`gitnya::webhooks.channel.secrets.${secret}`, undefined);
+
+            const iv = getIV();
+            secret = crypto.createCipheriv('aes-256-gcm', (await client.config.getOne('gitnya::webhooks.key')).padEnd(32), iv).update(decrypted, 'utf8').toString('base64');
+            const newId = crypto.createHash('md5').update(channelId).digest('base64url');
+            client.config.set(`gitnya::webhooks.channel.mappings.${newId}`, {
+                channelId, repo, secret,
+                iv: iv.toString('base64')
+            });
+            list = list.filter(v => v !== UUID);
+            list.push(newId);
+
+            client.logger.warn(`Found legacy storage: ${decrypted}. Converted to new style.`);
+            continue;
+        }
         const hash = crypto.createHash('md5').update(secret).digest('base64url');
         addWebhooks(secret, hash, channelId);
     }
+    client.config.set('gitnya::webhooks.channel.list', list);
 
     await client.connect()
     const basicPath = upath.join(__dirname, 'menu');
